69bf0cb6f12b30c4c892b1ed05de3ff7239de0b9
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-10-02 13:34:08 +0200

    Add Driver Class for Http Server

---
+++ src/main/java/httpServer/HttpServer.java
@@ -0,0 +1,14 @@
+package httpServer;
+
+/**
+ *
+ * Driver Class for Http Server
+ *
+ * */
+
+public class HttpServer {
+
+    public static void main (String[]args){
+        System.out.println("Server starting...");
+    }
+}
